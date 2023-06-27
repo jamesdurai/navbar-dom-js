f95c47444336bd359b10a34e06ea0f32589547bd
jamesdurai <jamesdurai@outlook.com>
2023-06-27 14:04:08 +0530

    nav bar section

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var navbarMenu = document.getElementById("navbarMenu");
+    navbarMenu.style.display = navbarMenu.style.display === "block" ? "none" : "block";
+  }
+  
+  
